9cf76c45f97f2728df131de822f53f65ba789ee8
ryotahoshino <75008198+ryotahoshino@users.noreply.github.com>
2023-02-05 22:36:35 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <header>
+      <div>
+        <a href="#">
+          <span>portfolio</span>
+        </a>
+        <nav>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default App
